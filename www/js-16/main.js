3e98a5ff0d6030aec8c676e4448a09de54710e33
harshittpandey <harshitpandey678@gmail.com>
2019-04-06 23:23:53 +0530

    Update main.js

--- www/js-16/main.js
+++ www/js-16/main.js
@@ -28,9 +28,9 @@ function paintToCanvas() {
         // console.log(pixels);
         // pixels= redEffect(pixels);
 
-        // pixels= rgbSplit(pixels);
+        pixels= rgbSplit(pixels);
 
-        // ctx.globalAlpha = 0.1;
+        ctx.globalAlpha = 0.1;
 
         pixels= greenScreen(pixels);
         ctx.putImageData(pixels, 0, 0);
@@ -65,8 +65,8 @@ function redEffect(pixels) {
 function rgbSplit(pixels) {
     for(let i=0; i<pixels.data.length; i+=4) {
         pixels.data[i- 150]= pixels.data[i+ 0] ;
-        pixels.data[i+ 500]= pixels.data[i+ 1] ;
-        pixels.data[i- 550]= pixels.data[i+ 2] ;
+        pixels.data[i+ 100]= pixels.data[i+ 1] ;
+        pixels.data[i- 150]= pixels.data[i+ 2] ;
     }
     return pixels;
 }
